be9cf3d3dc9ad6b23f0c706372c8af4a3b44e45b
Axel <axel.gantou@carnus.fr>
2025-11-04 14:20:37 +0100

    Apprentissage du C++, ajouts des premiers fichier.cpp

---
+++ Codes/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() { // Ou int main(int argc , char* argv [])
+    cout << "Hello world!" << endl;
+    // Ou std::cout << "Hello World!\n"; si on supprime la ligne 2
+return 0;
+}
