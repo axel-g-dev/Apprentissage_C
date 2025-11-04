b8889084a234e51b7df0b5f3e74a01c63bd04778
Axel <axel.gantou@carnus.fr>
2025-11-04 14:33:46 +0100

    Ajout de chaines_de_caracteres.cpp

---
+++ Codes/chaines_de_caracteres.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    string ville = "Rodez";
+    cout << "Longueur: " << ville.length() << endl;
+    cout << "Troisième caractère: " << ville[2] << endl;
+return 0;
+}
