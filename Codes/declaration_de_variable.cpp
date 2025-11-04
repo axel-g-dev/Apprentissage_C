b06066285598b8ff6accfe245de9c647b695812a
Axel <axel.gantou@carnus.fr>
2025-11-04 14:26:30 +0100

    Ajout de declaration_de_variable.cpp

---
+++ Codes/declaration_de_variable.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() { // Ou int main(int argc , char* argv [])
+    /* - Déclaration avec initialisation */
+    int var1 = 16; // entier valant 16
+    double var2 = 4.53; // réel valant 4.53
+    double erreur = 1.0e-12; // 10 puissance -12 (notation scientifique)
+    bool var3 = true; // booléen vrai
+    char var4 = 'a '; // caractère 'a '
+    string nom = "Carnus"; // chaîne de caractères "Carnus"
+    string chaine = "BTS CIEL"; // chaîne de caractères "BTS CIEL"
+    /* - Déclaration sans initialisation */
+    int a;
+    double x;
+    bool test;
+    string phrase;
+return 0;
+}
+
