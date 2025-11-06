a86205faf19bbd42a60428f351c1fc094f67a94b
Axel <axel.gantou@carnus.fr>
2025-11-06 10:24:16 +0100

    Ajout de la fonction addition & soustraction à prog1.cpp

--- Codes/Cours/prog1.cpp
+++ Codes/Cours/prog1.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int ma_fonction(int a, int b) {
-    return a + b;
-}
+// Déclaration (Prototype de ma fonction)
+int addition(int a, int b); 
+int soustraction(int a, int b);   
 
 int main() { 
     cout << "Programme" << endl;
-    int x; 
-    x = ma_fonction(10, 5);
-    cout << "Le résultat est : " << x << endl;
-return 0;
+    int x_soustraction = soustraction(10, 5); 
+    int x_addition = addition(10, 5);
+
+    cout << "Le résultat est : " << x_addition << endl;
+    cout << "Le résultat de la soustraction est : " << x_soustraction << endl;
+    return 0;
 }
 
+// Définition de ma fonction addition
+int addition(int a, int b) {
+    return a + b;
+}
+
+// Définition de ma fonction soustraction
+int soustraction(int a, int b) {
+    return a - b;
+}
